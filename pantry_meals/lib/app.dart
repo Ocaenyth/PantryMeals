7ead02ec50dbca28e0b5eab6532d49e7729ef543
Alex <debeaumont.alex@gmail.com>
2020-07-29 11:21:35 +0200

    + App is now stateful + Added app reload on localization change

--- pantry_meals/lib/app.dart
+++ pantry_meals/lib/app.dart
@@ -2,18 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:pantry_meals/localization/localizations_delegates.dart';
 import 'package:pantry_meals/localization/supported_locales.dart';
 import 'package:pantry_meals/pages/pantry/pantry_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AppState();
+
+  static void setLocale(BuildContext context, Locale newLocale) {
+    _AppState state = context.findAncestorStateOfType<_AppState>();
+
+    state.setLocale(newLocale);
+  }
+}
+
+class _AppState extends State<App> {
+  Locale locale;
+
   @override
   Widget build(BuildContext context) {
     final String _title = "Pantry Meals";
     return MaterialApp(
       localizationsDelegates: localizationsDelegates,
       supportedLocales: supportedLocales,
+      locale: locale,
       title: _title,
       home: PantryPage(),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this._fetchLocale().then((locale) {
+      setState(() {
+        this.locale = locale;
+        print(this.locale.languageCode);
+      });
+    });
+  }
+
+  _fetchLocale() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String languageCode = prefs.getString('language_code');
+
+    if (languageCode == null) {
+      languageCode = 'fr';
+    }
+
+    return Locale(languageCode, '');
+  }
+
+  setLocale(Locale newLocale) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('languageCode', newLocale.languageCode);
+
+    setState(() {
+      this.locale = newLocale;
+    });
   }
 }
