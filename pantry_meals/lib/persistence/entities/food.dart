8736c219e974edf3259844a0f57cc2d9f58e1382
Alex <debeaumont.alex@gmail.com>
2020-08-05 21:06:13 +0200

    [Fix food quantity] + Added a fix for food quantity (was not always an int)

--- pantry_meals/lib/persistence/entities/food.dart
+++ pantry_meals/lib/persistence/entities/food.dart
@@ -22,11 +22,13 @@ class Food {
   Food({this.id, this.barcode, this.name, this.quantity});
 
   factory Food.fromJson(Map<String, dynamic> json) {
+    var quantity = json['product']['product_quantity'] ?? 0;
+
     return Food(
       id: json['id'],
       barcode: json['code'],
       name: json['product']['product_name'],
-      quantity: json['product']['product_quantity'],
+      quantity: int.parse(quantity.toString()),
     );
   }
 }
